993864c6b40edefe82fb633beebb13a664ffa754
Xavier Perseguers <xavier@causal.ch>
2023-11-21 13:57:41 +0100

    [TASK] Add proper PHP dependency to ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,6 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
+            'php' => '7.4.0-8.2.99',
             'typo3' => '10.4.0-12.4.99',
         ],
         'conflicts' => [
