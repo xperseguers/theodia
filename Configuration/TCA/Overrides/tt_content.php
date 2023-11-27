f661318829aca911c236b68c10b355f662cc72c0
Xavier Perseguers <xavier@causal.ch>
2023-11-27 14:12:54 +0100

    [TASK] Implement standard PreviewRenderer for the plugins

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,6 +1,7 @@
 <?php
 defined('TYPO3') || die();
 
+// TODO: Migrate to ExtensionUtility::addPlugin() to have a custom CType instead of a "list_type"
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
     'theodia',
     'Event',
@@ -22,3 +23,12 @@ $pluginSignature = 'theodia_place';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,pages,recursive';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:theodia/Configuration/FlexForms/flexform_place.xml');
+
+/**
+ * Configure a custom preview renderer for the plugins
+ * @see https://docs.typo3.org/m/typo3/reference-coreapi/11.5/en-us/ApiOverview/ContentElements/CustomBackendPreview.html
+ */
+$GLOBALS['TCA']['tt_content']['types']['list']['previewRenderer']['theodia_event']
+    = \Causal\Theodia\Preview\EventPreviewRenderer::class;
+$GLOBALS['TCA']['tt_content']['types']['list']['previewRenderer']['theodia_place']
+    = \Causal\Theodia\Preview\PlacePreviewRenderer::class;
