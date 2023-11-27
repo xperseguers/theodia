0a8dcafa5214c33385942d12431be3d27888c9ae
Xavier Perseguers <xavier@causal.ch>
2023-11-27 16:22:22 +0100

    [TASK] Migrate registration of FlexForm for our plugins

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,35 +1,50 @@
 <?php
 defined('TYPO3') || die();
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin([
-    'LLL:EXT:theodia/Resources/Private/Language/locallang_db.xlf:plugins.event.title',
-    'theodia_event',
-    'theodia-default',
-], 'CType', 'theodia');
+$plugins = [
+    'theodia_event' => 'LLL:EXT:theodia/Resources/Private/Language/locallang_db.xlf:plugins.event.title',
+    'theodia_place' => 'LLL:EXT:theodia/Resources/Private/Language/locallang_db.xlf:plugins.place.title',
+];
+foreach ($plugins as $CType => $title) {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin([
+        $title,
+        $CType,
+        'theodia-default',
+    ], 'CType', 'theodia');
+}
 
-//$pluginSignature = 'theodia_event';
-//$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,pages,recursive';
-//$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    '*',
-    'FILE:EXT:theodia/Configuration/FlexForms/flexform_event.xml',
-    'theodia_event'
-);
+$flexForms = [
+    'theodia_event' => 'FILE:EXT:theodia/Configuration/FlexForms/flexform_event.xml',
+    'theodia_place' => 'FILE:EXT:theodia/Configuration/FlexForms/flexform_place.xml',
+];
+foreach ($flexForms as $CType => $flexForm) {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+        '*',
+        $flexForm,
+        $CType
+    );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin([
-    'LLL:EXT:theodia/Resources/Private/Language/locallang_db.xlf:plugins.place.title',
-    'theodia_place',
-    'theodia-default',
-], 'CType', 'theodia');
-
-//$pluginSignature = 'theodia_place';
-//$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,pages,recursive';
-//$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    '*',
-    'FILE:EXT:theodia/Configuration/FlexForms/flexform_place.xml',
-    'theodia_place'
-);
+    $GLOBALS['TCA']['tt_content']['types'][$CType]['showitem'] = '
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+            --palette--;;general,
+            --palette--;;headers,
+        --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.plugin,
+            pi_flexform,
+        --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+            --palette--;;frames,
+            --palette--;;appearanceLinks,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+            --palette--;;language,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+            --palette--;;hidden,
+            --palette--;;access,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+            categories,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+            rowDescription,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+    ';
+}
 
 /**
  * Configure a custom preview renderer for the plugins
