ca93b3750aa812609a85290420825b8062de8c57
Xavier Perseguers <xavier@causal.ch>
2023-11-30 13:30:22 +0100

    [TASK] Make categorization work in TYPO3 v12

--- Configuration/TCA/Overrides/tx_theodia_place.php
+++ Configuration/TCA/Overrides/tx_theodia_place.php
@@ -1,11 +1,19 @@
 <?php
 defined('TYPO3') || die();
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::makeCategorizable(
-    'theodia',
-    'tx_theodia_place',
-    'categories',
-    [
-        'position' => 'replace:categories',
-    ]
-);
+if ((new \TYPO3\CMS\Core\Information\Typo3Version())->getMajorVersion() >= 12) {
+    $GLOBALS['TCA']['tx_theodia_place']['columns']['categories'] = [
+        'config' => [
+            'type' => 'category'
+        ]
+    ];
+} else {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::makeCategorizable(
+        'theodia',
+        'tx_theodia_place',
+        'categories',
+        [
+            'position' => 'replace:categories',
+        ]
+    );
+}
