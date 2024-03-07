8acec397a67208b5122cf0867ba27284d199e3e6
Xavier Perseguers <xavier@causal.ch>
2024-03-07 10:04:24 +0100

    [BUGFIX] Configure CSP for loading tiles from OpenStreetMap

---
+++ Configuration/ContentSecurityPolicies.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types = 1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\Directive;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\Mutation;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\MutationCollection;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\MutationMode;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\Scope;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\SourceScheme;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\UriValue;
+use TYPO3\CMS\Core\Type\Map;
+
+return Map::fromEntries([
+    // Provide declarations for the Backend
+    Scope::backend(),
+
+    new MutationCollection(
+        new Mutation(
+            MutationMode::Extend,
+            Directive::ImgSrc,
+            SourceScheme::data,
+            new UriValue('https://*.tile.openstreetmap.org')
+        )
+    )
+]);
