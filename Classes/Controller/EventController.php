54123444d3683c61b9f4eae5ef7eac2bbeab7318
Xavier Perseguers <xavier@causal.ch>
2024-06-13 16:51:51 +0200

    [TASK] Allow up to 200 events to be displayed and streamline FlexForm accordingly

--- Classes/Controller/EventController.php
+++ Classes/Controller/EventController.php
@@ -28,7 +28,7 @@ use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
 class EventController extends ActionController
 {
     protected const CACHE_LIFETIME = 14400; /* 4 hours */
-    protected const MAX_EVENTS = 100;
+    protected const MAX_EVENTS = 200;
 
     /**
      * @var TheodiaOrg
