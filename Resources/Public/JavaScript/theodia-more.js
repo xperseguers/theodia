eaf1c406bfd0077b1862717b93823c746550d287
Hasan Shaker <shaker@hemmer.ch>
2024-06-14 10:19:11 +0200

    [BUGFIX] Put events in targetEl if response.json has data

--- Resources/Public/JavaScript/theodia-more.js
+++ Resources/Public/JavaScript/theodia-more.js
@@ -32,13 +32,17 @@ document.addEventListener('DOMContentLoaded', function () {
                 body: formData
             }).then(response => response.json())
                 .then(data => {
+                    if (data.html) {
+                        targetEl.innerHTML += data.html;
+                        targetEl.dataset.events = data.numberEvents;
+                    }
                     if (!data.hasMore) {
                         targetEl.innerHTML = data.html;
                         targetEl.dataset.events = data.numberEvents;
                         // Hide the show more button if not needed anymore
                         element.style.display = 'none';
                     }
-                });
+                }).catch(error => console.error('Error:', error));
         });
     });
 });
