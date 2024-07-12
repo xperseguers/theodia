d07b46aede3d5e33e74cb67e96a9e741c347386e
Xavier Perseguers <xavier@causal.ch>
2024-07-12 09:47:13 +0200

    [TASK] Migrate registration with IconRegistry to Configuration/Icons.php

--- ext_tables.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('TYPO3') || die();
-
-(static function (string $_EXTKEY) {
-    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-    $iconRegistry->registerIcon('theodia-default', \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class, [
-        'source' => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/Extension.svg',
-    ]);
-})('theodia');
